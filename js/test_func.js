f004d97f452b2b799b686e232b17c630fef93bf2
DyanByta <hyacinthdby@outlook.com>
2019-09-25 18:29:15 +0800

    更新部分外观

--- js/test_func.js
+++ js/test_func.js
@@ -2,6 +2,8 @@
  *Created by Dyan at 2019/6/3 18:15
  */
 
+var title_origin = "🐱幻想猫舍🐱";
+
 //绑定点击事件
 $(document).ready(function () {
     //功能选择按钮——通用操作
@@ -12,9 +14,11 @@ $(document).ready(function () {
         fc_selector.empty();
         return fc_selector;
     }
-    //功能选择——论文阅读辅助
-    $("#paper_read").on("click", function () {
-        fc_selector = class_replace(this.id);
+    //论文阅读辅助
+    function paper_read(){
+        //改变标题
+        document.title = document.getElementById("paper_read").innerText + " - " + title_origin;
+        let fc_selector = class_replace(this.id);
         fc_selector.append(
             "<textarea class=\"text_blank\" id=\"raw_text\" placeholder=\"在这里输入原始文本\"></textarea>\n" +
             "<div class=\"buttons\">\n" +
@@ -35,19 +39,21 @@ $(document).ready(function () {
             $("#raw_text").val("");
             $("#rul_text").val("");
         })
-    });
-    //功能选择——智能测试模块
-    $("#intel_test").on("click", function () {
-        fc_selector = class_replace(this.id);
+    }
+    //智能测试模块
+    function intel_test(){
+        //改变标题
+        document.title = document.getElementById("intel_test").innerText + " - " + title_origin;
+        let fc_selector = class_replace(this.id);
         fc_selector.append(
-            "<div id=\"chat_blank\">\n" +
-            "    <div class=\"profile\"><img src=\"image/灵.jpg\" alt=\"主机\"></div>\n" +
+            "<div class=\"border_radius\" id=\"chat_blank\">\n" +
+            "    <div class=\"profile\"><img src=\"image/喵.jpg\" alt=\"主机\"></div>\n" +
             "    <div class=\"chat_item\">\n" +
-            "        <div class=\"chat_left\">你好</div><div class=\"clear\"></div>\n" +
+            "        <div class=\"chat_left border_radius\">你好</div>\n" +
             "    </div>\n" +
             "</div>\n" +
             "<textarea id=\"text_blank\" placeholder=\"说点什么\"></textarea>\n" +
-            "<div class=\"align_center_vertical\" id=\"send_text\">发送</div>"
+            "<div class=\"align_center_vertical border_radius\" id=\"send_text\">发送</div>"
         );
         document.getElementById("text_blank").focus();
         //将输入文本传入系统，生成回复并显示聊天效果
@@ -60,9 +66,9 @@ $(document).ready(function () {
                 // let chat_selector = $("#chat_blank");
                 $("#chat_blank").append(
                     "<div class=\"chat_item\">\n" +
-                    "    <div class=\"chat_right\">" + text + "</div><div class=\"clear\"></div>\n" +
+                    "    <div class=\"chat_right border_radius\">" + text + "</div>\n" +
                     "</div>\n" +
-                    "<div class=\"profile\"><img src=\"image/空.png\" alt=\"用户\"></div>"
+                    "<div class=\"profile\"><img src=\"image/猫.jpg\" alt=\"用户\"></div>"
                 );
                 //获取回复
                 chart_reply(text);
@@ -80,9 +86,9 @@ $(document).ready(function () {
             let reply_text = reply(text);
             //输出聊天效果——系统
             $("#chat_blank").append(
-                "<div class=\"profile\"><img src=\"image/灵.jpg\" alt=\"主机\"></div>" +
+                "<div class=\"profile\"><img src=\"image/喵.jpg\" alt=\"主机\"></div>" +
                 "<div class=\"chat_item\">\n" +
-                "    <div class=\"chat_left\">" + reply_text + "</div><div class=\"clear\"></div>\n" +
+                "    <div class=\"chat_left border_radius\">" + reply_text + "</div>\n" +
                 "</div>\n"
             );
         }
@@ -95,7 +101,35 @@ $(document).ready(function () {
                 chart_write();
             }
         })
+    }
+    function test(){
+        alert("开发中……");
+    }
+    //初始功能选择
+    $(".func_select").on("click", function () {
+        let func_selector = $(".origin_container");
+        func_selector.empty();
+        func_selector.append(
+            "<div class=\"func_list border_radius\">\n" +
+            "    <ul class=\"border_radius\" id=\"func_select\">返回功能选择</ul>\n" +
+            "    <ul class=\"border_radius\" id=\"paper_read\">论文阅读辅助</ul>\n" +
+            "    <ul class=\"border_radius\" id=\"intel_test\">智能测试模块</ul>\n" +
+            "    <ul class=\"border_radius\" id=\"test\">测试功能</ul>\n" +
+            "</div>\n" +
+            "<div class=\"func_container border_radius\"></div>"
+        );
+        //功能选择——返回功能选择
+        $("#func_select").on("click", function () {
+            location.reload();
+        });
+        //功能选择——论文阅读辅助
+        $("#paper_read").on("click", paper_read);
+        //功能选择——智能测试模块
+        $("#intel_test").on("click", intel_test);
+        //功能选择——测试功能
+        $("#test").on("click", test);
+        //跳转到所选功能
+        eval(this.id + "()");
     });
-
 });
 
